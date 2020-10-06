6b158ea6e64e2f05a5b2eecc87f80ec6916d29ae
Zedxel <Thomas.Temby@hotmail.com>
2020-10-06 16:11:21 +1030

    Added both SQL files

---
+++ database/MCP_DB_data.sql
@@ -0,0 +1,77 @@
+INSERT INTO Customer VALUES ('0001', 'Mr', 'Warren', 'Peece');
+INSERT INTO Customer VALUES ('3579', 'Mr', 'Hugo', 'First');
+INSERT INTO Customer VALUES ('3699', 'Mr', 'Juan', 'Anatu');
+INSERT INTO Customer VALUES ('4887', 'Miss', 'Lauren', 'Order');
+INSERT INTO Customer VALUES ('4888', 'Mrs', 'Fay', 'Tality');
+INSERT INTO Customer VALUES ('7001', 'Mrs', 'Theresa', 'Crowd');
+INSERT INTO Customer VALUES ('8650', 'Ms', 'Jean', 'Poole');
+INSERT INTO Customer VALUES ('2700', 'Mr', 'Walter', 'Wall');
+INSERT INTO Customer VALUES ('0183', 'Mr', 'Dale', 'Neverno');
+INSERT INTO Customer VALUES ('0010', 'Miss', 'Lotta', 'Noyes');
+
+INSERT INTO CustResAddr VALUES ('0001', '34 Yellow Brick Road', 'Asbury Park', '21047', '03548 621 6711', 'wp67@home.today');
+INSERT INTO CustResAddr VALUES ('3579', '3 Abbey Road', 'Mansfield', '19045', '84566 004 7470', 'jpgr4@lunch.right.now');
+INSERT INTO CustResAddr VALUES ('3699', '67 Copperhead Road', 'Strawberry Fields', '04015', '06444 202 1902', 'iam@work.need.cash');
+INSERT INTO CustResAddr VALUES ('4887', '231 Thunder Road', 'Backwater', '14117', '03656 333 0101', 'fee@car.for.sale');
+INSERT INTO CustResAddr VALUES ('4888', 'Lot 351 Telegraph Road', 'Ghosttown', '62357', '09852 007 1852', 'stay@hotel.for.work');
+INSERT INTO CustResAddr VALUES ('7001', 'U4, 56 Longnwinding Road', 'Arboria', '23995', '04512 707 1066', 'crowd@the.concert');
+INSERT INTO CustResAddr VALUES ('8650', '529 Country Road', 'Kokomo', '33827', '01202 999 1972', 'jp007@thepub.quiz');
+INSERT INTO CustResAddr VALUES ('2700', '26 52nd Street', 'Suffragette City', '45674', '02987 351 1888', 'wally@thepark.hiding');
+INSERT INTO CustResAddr VALUES ('0183', '461 Ocean Boulevard', 'Blueberry Hill', '19934', '06951 665 6655', 'neverno@the.bigsmoke');
+INSERT INTO CustResAddr VALUES ('0010', '761A Gasoline Alley', 'Jurassic Park', '16637', '09511 000 2512', 'noysey@the.midnite.hour');
+
+INSERT INTO CustDelAddr VALUES ('0001', '34 Yellow Brick Road', 'Asbury Park', '21047', ""); 
+INSERT INTO CustDelAddr VALUES ('3579', '3 Abbey Road', 'Mansfield', '19045', 'Stop putting it in the letterbox, it doesnt fit');
+INSERT INTO CustDelAddr VALUES ('3699', 'U7 86 Sterling Grove', 'Steepe Hills', '65957', 'Road floods in the winter - take a pair of wellies');
+INSERT INTO CustDelAddr VALUES ('4887', '3266 Panorama Circuit', 'China Grove', '78219', 'Please leave parcel with sister at number 12');
+INSERT INTO CustDelAddr VALUES ('4888', 'Lot 351 Telegraph Road', 'Ghosttown', '62357', 'Dont ring the bell, youll annoy the dog');
+INSERT INTO CustDelAddr VALUES ('7001', 'U4, 56 Longnwinding Road', 'Arboria', '23995', 'Tree Men; do not send Gordon to deliver');
+INSERT INTO CustDelAddr VALUES ('8650', '529 Country Road', 'Kokomo', '33827', "");
+INSERT INTO CustDelAddr VALUES ('2700', '26 52nd Street', 'Suffragette City', '45674', "");
+INSERT INTO CustDelAddr VALUES ('0183', 'Level 82 The Squeegee', 'Hillen Dale', '24006', 'The middle lift only goes to floor 20');
+INSERT INTO CustDelAddr VALUES ('0010', '761A Gasoline Alley', 'Jurassic Park', '16637', "");
+
+INSERT INTO Product VALUES ('drag6510', 'Dragon Scale', 'Green dragon scale - medium size', 85.35, 266); 
+INSERT INTO Product VALUES ('unic1100', 'Unicorn horn', '15" long, no cracks', 480.00, 8);
+/*INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (
+INSERT INTO Product VALUES (*/
+
+INSERT INTO Card VALUES ('0001', '0005 21568 78562 0011');
+INSERT INTO Card VALUES ('3579', '0005 55684 32100 1100');
+INSERT INTO Card VALUES ('3699', '0005 88844 36963 0110');
+INSERT INTO Card VALUES ('4887', '0005 15937 46820 1010');
+INSERT INTO Card VALUES ('4888', '0110 12389 98721 0100');
+INSERT INTO Card VALUES ('7001', '0110 64951 22881 0010');
+INSERT INTO Card VALUES ('8650', '0105 36415 85258 0001');
+INSERT INTO Card VALUES ('2700', '0105 21181 34729 1001');
+INSERT INTO Card VALUES ('0183', '0105 13815 45229 1000');
+INSERT INTO Card VALUES ('0010', '0105 67990 18723 0101');
+
+INSERT INTO Cart VALUES ('0000556', 'drag6510', 85.35, 18, 1536.30, '2700');
+INSERT INTO Cart VALUES ('0000599', 'drag6510', 85.35, 30, 2560.50, '0010');
+INSERT INTO Cart VALUES ('0000610', 'unic1100', 480.00, 1, 480.00, '3579');
+/*INSERT INTO Cart VALUES (
+INSERT INTO Cart VALUES (
+INSERT INTO Cart VALUES (
+INSERT INTO Cart VALUES (
+INSERT INTO Cart VALUES (
+INSERT INTO Cart VALUES (
+INSERT INTO Cart VALUES (
+
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (
+INSERT INTO Invoice VALUES (*/
