bb90665e97271b77053e0832616c4ec33cebd7db
Jaredgib <69283235+Jaredgib@users.noreply.github.com>
2020-10-12 16:22:37 +1030

    Current DATABASE

---
+++ database/MCP_Bld.sql
@@ -0,0 +1,76 @@
+SET @@AUTOCOMMIT = 1;
+
+DROP DATABASE IF EXISTS MCP;
+CREATE DATABASE MCP;
+
+USE MCP;
+
+CREATE TABLE Customer(
+	custId varchar(8) NOT NULL,
+	title varchar(8),
+	fname varchar(40) NOT NULL,
+	lname varchar(40) NOT NULL,
+	PRIMARY KEY(custId)
+) AUTO_INCREMENT = 1;
+
+CREATE TABLE CustResAddr(
+	custId varchar(8),
+	Street varchar(45),
+	Suburb varchar(45),
+	Postcode char(5) NOT NULL DEFAULT 1001,
+	phone varchar(14) NOT NULL DEFAULT 555,
+	email varchar(100),
+	PRIMARY KEY (custId),
+	FOREIGN KEY(CustId) REFERENCES Customer(custId)
+);
+
+CREATE TABLE CustDelAddr(
+	custId varchar(8) NOT NULL,
+	Street varchar(45),
+	Suburb varchar(45),
+	Postcode char(5),
+	deliveryNote varchar(300),
+	PRIMARY KEY(custId),
+	FOREIGN KEY(custId) REFERENCES Customer(custId)
+);
+
+CREATE TABLE Product (
+	stockNum varchar(10) NOT NULL,
+	productName varchar(25),
+	description varchar(300),
+	unitCost decimal(6,2),
+	stockQty int(8),
+	PRIMARY KEY(stockNum)
+);
+
+CREATE TABLE Card (
+	custId varchar(8) NOT NULL,
+	cardNum	char(21) NOT NULL,
+	PRIMARY KEY(custId),
+	FOREIGN KEY(custId) REFERENCES Customer(custId)
+);
+
+CREATE TABLE Cart (
+	cartId varchar(10) NOT NULL,
+	stockNum varchar(10),
+	unitCost decimal(6,2),
+	qty int(3),
+	totalCost decimal(8,2),
+	custId varchar(8) NOT NULL,
+	PRIMARY KEY(cartId, stockNum),
+	FOREIGN KEY(custId) REFERENCES Customer(custId),
+	FOREIGN KEY(stockNum) REFERENCES Product(stockNum)
+);
+
+CREATE TABLE Invoice (
+	invoiceId char(10) NOT NULL,
+	custId varchar(8)NOT NULL,
+	invoiceDate	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	deliveryFee decimal(5,2),
+	finCost decimal(8,2) NOT NULL,
+	PRIMARY KEY (invoiceId),
+	FOREIGN KEY(custId) REFERENCES Customer(custId)
+);
+
+CREATE user IF NOT EXISTS dbadmin@localhost;
+GRANT all privileges ON SalesDB TO dbadmin@localhost;
---
+++ database/MCP_data.sql
@@ -0,0 +1,78 @@
+INSERT INTO Customer VALUES ('0001', 'Mr', 'Warren', 'Peece');
+INSERT INTO Customer VALUES ('3579', 'Mr', 'Hugo', 'First');
+INSERT INTO Customer VALUES ('3699', 'Mr', 'Juan', 'Anatu');
+INSERT INTO Customer VALUES ('4887', 'Miss', 'Lauren', 'Order');
+INSERT INTO Customer VALUES ('4888', 'Mrs', 'Fay', 'Tality');
+INSERT INTO Customer VALUES ('7001', 'Mrs', 'Theresa', 'Crowd');
+INSERT INTO Customer VALUES ('8650', 'Ms', 'Jean', 'Poole');
+INSERT INTO Customer VALUES ('2700', 'Mr', 'Walter', 'Wall');
+INSERT INTO Customer VALUES ('0183', 'Mr', 'Dale', 'Neverno');
+INSERT INTO Customer VALUES ('0010', 'Miss', 'Lotta', 'Noyes');
+
+INSERT INTO CustResAddr VALUES ('0001', '34 Yellow Brick Road', 'Asbury Park', '21047', '03548 621 6711', 'wp67@home.today');
+INSERT INTO CustResAddr VALUES ('3579', '3 Abbey Road', 'Mansfield', '19045', '84566 004 7470', 'jpgr4@lunch.right.now');
+INSERT INTO CustResAddr VALUES ('3699', '67 Copperhead Road', 'Strawberry Fields', '04015', '06444 202 1902', 'iam@work.need.cash');
+INSERT INTO CustResAddr VALUES ('4887', '231 Thunder Road', 'Backwater', '14117', '03656 333 0101', 'fee@car.for.sale');
+INSERT INTO CustResAddr VALUES ('4888', 'Lot 351 Telegraph Road', 'Ghosttown', '62357', '09852 007 1852', 'stay@hotel.for.work');
+INSERT INTO CustResAddr VALUES ('7001', 'U4, 56 Longnwinding Road', 'Arboria', '23995', '04512 707 1066', 'crowd@the.concert');
+INSERT INTO CustResAddr VALUES ('8650', '529 Country Road', 'Kokomo', '33827', '01202 999 1972', 'jp007@thepub.quiz');
+INSERT INTO CustResAddr VALUES ('2700', '26 52nd Street', 'Suffragette City', '45674', '02987 351 1888', 'wally@thepark.hiding');
+INSERT INTO CustResAddr VALUES ('0183', '461 Ocean Boulevard', 'Blueberry Hill', '19934', '06951 665 6655', 'neverno@the.bigsmoke');
+INSERT INTO CustResAddr VALUES ('0010', '761A Gasoline Alley', 'Jurassic Park', '16637', '09511 000 2512', 'noysey@the.midnite.hour');
+
+INSERT INTO CustDelAddr VALUES ('0001', '34 Yellow Brick Road', 'Asbury Park', '21047', ""); 
+INSERT INTO CustDelAddr VALUES ('3579', '3 Abbey Road', 'Mansfield', '19045', 'Stop putting it in the letterbox, it doesnt fit');
+INSERT INTO CustDelAddr VALUES ('3699', 'U7 86 Sterling Grove', 'Steepe Hills', '65957', 'Road floods in the winter - take a pair of wellies');
+INSERT INTO CustDelAddr VALUES ('4887', '3266 Panorama Circuit', 'China Grove', '78219', 'Please leave parcel with sister at number 12');
+INSERT INTO CustDelAddr VALUES ('4888', 'Lot 351 Telegraph Road', 'Ghosttown', '62357', 'Don\'t ring the bell, you\'ll annoy the dog');
+INSERT INTO CustDelAddr VALUES ('7001', 'U4, 56 Longnwinding Road', 'Arboria', '23995', 'Tree Men; do not send Gordon to deliver');
+INSERT INTO CustDelAddr VALUES ('8650', '529 Country Road', 'Kokomo', '33827', "");
+INSERT INTO CustDelAddr VALUES ('2700', '26 52nd Street', 'Suffragette City', '45674', "");
+INSERT INTO CustDelAddr VALUES ('0183', 'Level 82 The Squeegee', 'Hillen Dale', '24006', 'The middle lift only goes to floor 20');
+INSERT INTO CustDelAddr VALUES ('0010', '761A Gasoline Alley', 'Jurassic Park', '16637', "");
+
+INSERT INTO Product VALUES ('drag6510', 'Dragon scale', 'Bronze dragon scale - medium size', 65.35, 266); 
+INSERT INTO Product VALUES ('unic1100', 'Unicorn horn', '15" long, no cracks', 480.00, 8);
+INSERT INTO Product VALUES ('wyvn4490', 'Wyvern talon', '8" long, curved, very sharp', 240.50, 154);
+INSERT INTO Product VALUES ('grif2360', 'Griffon beak', 'Yellow beak, upper, 500gms', 360.80, 278);
+INSERT INTO Product VALUES ('grif2340', 'Griffon beak', 'Yellow beak, lower, 300gms', 210.00, 410);
+INSERT INTO Product VALUES ('grif2350', 'Griffon beak', 'Yellow beak, upper and lower set', 630.30, 56);
+INSERT INTO Product VALUES ('drag6550', 'Dragon teeth', 'Dragon teeth, bright white, 12-16 inches long', 297.60, 230);
+INSERT INTO Product VALUES ('cent8020', 'Centaur hoof', 'Centaur hooves, cleaned, min 1.5kg', 600.00, 36);
+INSERT INTO Product VALUES ('drag6490', 'Dragon talon', 'Eastern dragon, 5 inch talons, curved, clean', 410.45, 370);
+INSERT INTO Product VALUES ('merm3500', 'Mermaid scale', 'Different colors, kept fresh in seawater', 10.00, 1810);
+INSERT INTO Product VALUES ('phnx7350', 'Phoenix beak', 'Upper and lower beaks, some scorch marks', 1200.00, 6);
+INSERT INTO Product VALUES ('basi9550', 'Basilisk fang', 'Large fangs, venom removed, clean', 100.00, 580);
+
+INSERT INTO Card VALUES ('0001', '0005 21568 78562 0011');
+INSERT INTO Card VALUES ('3579', '0005 55684 32100 1100');
+INSERT INTO Card VALUES ('3699', '0005 88844 36963 0110');
+INSERT INTO Card VALUES ('4887', '0005 15937 46820 1010');
+INSERT INTO Card VALUES ('4888', '0110 12389 98721 0100');
+INSERT INTO Card VALUES ('7001', '0110 64951 22881 0010');
+INSERT INTO Card VALUES ('8650', '0105 36415 85258 0001');
+INSERT INTO Card VALUES ('2700', '0105 21181 34729 1001');
+INSERT INTO Card VALUES ('0183', '0105 13815 45229 1000');
+INSERT INTO Card VALUES ('0010', '0105 67990 18723 0101');
+
+INSERT INTO Cart VALUES ('0000556', 'drag6510', 85.35, 18, 1536.30, '2700');
+INSERT INTO Cart VALUES ('0000556', 'cent8020', 600.00, 2, 1200.00, '2700');
+INSERT INTO Cart VALUES ('0000599', 'drag6510', 85.35, 30, 2560.50, '0010');
+INSERT INTO Cart VALUES ('0000610', 'unic1100', 480.00, 1, 480.00, '3579');
+INSERT INTO Cart VALUES ('0000612', 'wyvn4490', 240.50, 2, 481.00, '7001');
+INSERT INTO Cart VALUES ('0000612', 'merm3500', 10.00, 20, 200.00, '7001');
+INSERT INTO Cart VALUES ('0000612', 'grif2340', 210.00, 4, 840.00, '7001');
+INSERT INTO Cart VALUES ('0000613', 'grif2350', 630.30, 1, 630.30, '4888');
+INSERT INTO Cart VALUES ('0000613', 'wyvn4490', 240.50, 2, 481.00, '4888');
+INSERT INTO Cart VALUES ('0000613', 'cent8020', 600.00, 4, 2400.00, '4888');
+INSERT INTO Cart VALUES ('0000614', 'phnx7350', 1200.00, 1, 1200.00, '3699');
+INSERT INTO Cart VALUES ('0000616', 'basi9550', 100.00, 4, 400.00, '0010');
+INSERT INTO Cart VALUES ('0000616', 'drag6550', 297.60, 2, 595.20, '0010');
+
+INSERT INTO Invoice VALUES ('MCP00556', '2700', '2010-10-12', 20.00, 2756.30);
+INSERT INTO Invoice VALUES ('MCP00599', '0010', '2010-10-12', 20.00, 2580.00);
+INSERT INTO Invoice VALUES ('MCP00610', '3579', '2010-10-13', 20.00, 480.00);
+INSERT INTO Invoice VALUES ('MCP00612', '7001', '2010-11-08', 20.00, 1541.00);
+INSERT INTO Invoice VALUES ('MCP00613', '4888', '2010-11-21', 20.00, 3531.30);
+INSERT INTO Invoice VALUES ('MCP00614', '3699', '2010-12-15', 20.00, 1220.00);
+INSERT INTO Invoice VALUES ('MCP00616', '0010', '2011-01-06', 20.00, 1015.00);
