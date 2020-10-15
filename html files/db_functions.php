a0696ea6c3e0215df16c512244a6f3efa6051a11
Jaredgib <gibs0036@flinders.edu.au>
2020-10-15 17:34:29 +1030

    check_qty/add_to_cart - Working

--- html files/db_functions.php
+++ html files/db_functions.php
@@ -1,9 +1,44 @@
 <?php
 
-function add_to_cart() {
+// Function that returns the stockQty from the database of a selected item
+// and checks to see if the store has sufficient product before sale
+function check_qty($conn, $stockNum, $qty) {
+    $query = "SELECT stockQty FROM product WHERE stockNum=?";
+
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "s", $stockNum);
+
+    if (mysqli_stmt_execute($stmt)) {
+        mysqli_stmt_bind_result($stmt, $stockQty);
+        mysqli_stmt_fetch($stmt);
+
+        if ($stockQty < $qty) {
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+    mysqli_stmt_close($stmt);
     
+    echo mysqli_error($conn);
+    mysqli_close($conn);
 }
 
+// Function to add items into a cart as well as custId
+function add_to_cart($conn, $custId, $stockNum, $unitCost, $qty) {
+    $totalCost = $unitCost * $qty;
+    $query = "INSERT INTO cart(stockNum, unitcost, qty, totalCost custId)";
 
+    $stmt = mysqli_prepare($conn, $query);
+
+    if ($stmt) {
+        $outcome = mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+        return $outcome;
+
+    } else {
+        return FALSE;
+    }
+}
 
 ?>
