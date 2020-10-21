c2877bceb8c31e2d3dab58877fbed8918bbb0ca3
Zedxel <Thomas.Temby@hotmail.com>
2020-10-21 16:08:25 +1030

    Changed 'Teeth/Fangs' categories to just 'Fangs'

--- database/MCP_data.sql
+++ database/MCP_data.sql
@@ -48,12 +48,12 @@ INSERT INTO Product VALUES ('wyvn4490', 'Wyvern talon', '8" long, curved, very s
 INSERT INTO Product VALUES ('grif2360', 'Griffon beak', 'Yellow beak, upper, 500gms', 360.80, 278, 'Beak');
 INSERT INTO Product VALUES ('grif2340', 'Griffon beak', 'Yellow beak, lower, 300gms', 210.00, 410, 'Beak');
 INSERT INTO Product VALUES ('grif2350', 'Griffon beak', 'Yellow beak, upper and lower set', 630.30, 56, 'Beak');
-INSERT INTO Product VALUES ('drag6550', 'Dragon teeth', 'Dragon teeth, bright white, 12-16 inches long', 297.60, 230, 'Teeth/Fangs');
+INSERT INTO Product VALUES ('drag6550', 'Dragon teeth', 'Dragon teeth, bright white, 12-16 inches long', 297.60, 230, 'Fangs');
 INSERT INTO Product VALUES ('cent8020', 'Centaur hoof', 'Centaur hooves, cleaned, min 1.5kg', 600.00, 36, 'Hoof');
 INSERT INTO Product VALUES ('drag6490', 'Dragon talon', 'Eastern dragon, 5 inch talons, curved, clean', 410.45, 370, 'Talon');
 INSERT INTO Product VALUES ('merm3500', 'Mermaid scale', 'Different colors, kept fresh in seawater', 10.00, 1810, 'Scale');
 INSERT INTO Product VALUES ('phnx7350', 'Phoenix beak', 'Upper and lower beaks, some scorch marks', 1200.00, 6, 'Beak');
-INSERT INTO Product VALUES ('basi9550', 'Basilisk fang', 'Large fangs, venom removed, clean', 100.00, 580, 'Teeth/Fangs');
+INSERT INTO Product VALUES ('basi9550', 'Basilisk fang', 'Large fangs, venom removed, clean', 100.00, 580, 'Fangs');
 
 INSERT INTO Card VALUES ('0001', '0005 21568 78562 0011');
 INSERT INTO Card VALUES ('0002', '0005 55684 32100 1100');
